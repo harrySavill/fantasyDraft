0d8565a731d226d536dde21da7b6eba0701889be
Harry Savill <160655325+harrySavill@users.noreply.github.com>
2026-09-09 23:58:09 +0100

    Update PRICE_BIAS_STRENGTH to 4.8

--- src/DraftBoard.jsx
+++ src/DraftBoard.jsx
@@ -5,7 +5,7 @@ import PlayerCard from './PlayerCard'
 
 const POSITION_ORDER = ['GK', 'DEF', 'MID', 'FWD']
 
-const PRICE_BIAS_STRENGTH = 4.5
+const PRICE_BIAS_STRENGTH = 4.8
 const FETCH_PAGE_SIZE = 1000
 
 async function fetchAllPlayerSeasons() {
