3fe04f05e39abbf298a30bdfd8961a3e9483e6db
Harry Savill <160655325+harrySavill@users.noreply.github.com>
2026-09-09 23:50:36 +0100

    Update DraftBoard.jsx
    adjusting bias for pricing

--- src/DraftBoard.jsx
+++ src/DraftBoard.jsx
@@ -5,7 +5,7 @@ import PlayerCard from './PlayerCard'
 
 const POSITION_ORDER = ['GK', 'DEF', 'MID', 'FWD']
 
-const PRICE_BIAS_STRENGTH = 5
+const PRICE_BIAS_STRENGTH = 3.8
 const FETCH_PAGE_SIZE = 1000
 
 async function fetchAllPlayerSeasons() {
